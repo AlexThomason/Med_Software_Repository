1821c7658030f423a182057a8a24ea478635f989
Alex Thomason <AlexThomason@Alexs-MacBook-Pro-7.local>
2021-08-30 12:44:20 -0400

    Initial commit of blood_calculator.py - added interface command

---
+++ blood_calculator.py
@@ -0,0 +1,12 @@
+# blood_calculator.py
+# Author: Alex Thomason
+
+def interface():
+    print("Blood Calculator")
+    print("Make a choice")
+    print("9-Quit")
+    choice = input("Make a choice: ")
+    print(choice)
+    return choice
+
+interface():
