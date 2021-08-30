a85bd35d14b9b4938bcfb464b8940f8e54e53a8d
Alex Thomason <AlexThomason@Alexs-MacBook-Pro-7.local>
2021-08-30 12:50:23 -0400

    modified the code so it quites when the number 9 is entered

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,10 +3,18 @@
 
 def interface():
     print("Blood Calculator")
-    print("Make a choice")
-    print("9-Quit")
-    choice = input("Make a choice: ")
+    keep_running = True
+    while keep_running:
+        print("Make a choice (number)")
+        print("9-Quit")
+        choice = int(input("Make a choice: "))
+        print(type(choice))
+
+        if choice == 9:
+            keep_running = False
+
+
     print(choice)
     return choice
 
-interface():
+interface()
